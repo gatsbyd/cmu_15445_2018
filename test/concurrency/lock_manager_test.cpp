d7aa7f8743e7335f3883779666405bfbde568fe2
daihaonan <1240184197@qq.com>
2019-04-25 17:12:49 +0800

    cause:add some test for lock manager solution:done

--- test/concurrency/lock_manager_test.cpp
+++ test/concurrency/lock_manager_test.cpp
@@ -40,4 +40,53 @@ TEST(LockManagerTest, BasicTest) {
   t0.join();
   t1.join();
 }
+
+TEST(LockManagerTest, SharedAndExclusiveTest) {
+  LockManager lock_mgr{false};
+  TransactionManager txn_mgr{&lock_mgr};
+  RID rid_a{0, 0};
+  int account_a = 100;
+  RID rid_b{0, 1};
+  int account_b = 200;
+  int total = 0;
+
+  // 不可能出现死锁
+  std::thread t0([&](int account_a, int account_b) {
+    Transaction txn(0);
+    bool res = lock_mgr.LockExclusive(&txn, rid_a);
+    EXPECT_EQ(res, true);
+    EXPECT_EQ(txn.GetState(), TransactionState::GROWING);
+    account_a += 50;
+    res = lock_mgr.LockExclusive(&txn, rid_b);
+    EXPECT_EQ(res, true);
+    EXPECT_EQ(txn.GetState(), TransactionState::GROWING);
+    account_b -= 50;
+    lock_mgr.Unlock(&txn, rid_a);
+    EXPECT_EQ(txn.GetState(), TransactionState::SHRINKING);
+    lock_mgr.Unlock(&txn, rid_b);
+    EXPECT_EQ(txn.GetState(), TransactionState::SHRINKING);
+  }, account_a, account_b);
+  std::thread t1([&] {
+    Transaction txn(1);
+    bool res = lock_mgr.LockShared(&txn, rid_a);
+    EXPECT_EQ(res, true);
+    EXPECT_EQ(txn.GetState(), TransactionState::GROWING);
+
+    res = lock_mgr.LockShared(&txn, rid_b);
+    EXPECT_EQ(res, true);
+    EXPECT_EQ(txn.GetState(), TransactionState::GROWING);
+
+    total = account_a + account_b;
+
+    lock_mgr.Unlock(&txn, rid_a);
+    EXPECT_EQ(txn.GetState(), TransactionState::SHRINKING);
+    lock_mgr.Unlock(&txn, rid_b);
+    EXPECT_EQ(txn.GetState(), TransactionState::SHRINKING);
+  });
+
+  t0.join();
+  t1.join();
+  EXPECT_EQ(300, total);
+  EXPECT_NE(350, total);
+}
 } // namespace cmudb
